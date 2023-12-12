6148bc5eb48a1ca49da73e1dd006404d1d701b1e
Dean Chap <deanchaps@gmail.com>
2023-12-11 19:15:35 -0700

    Model commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold underline'> 
-        Tesla clone  
-      </h1>
+    <div
+      className="
+      bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Desktop-NA.jpg')] 
+      lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Desktop-NA.jpg')] 
+      h-screen bg-cover bg-center "
+    >
+      <Navbar />
+      <Model />
     </div>
   );
 }
