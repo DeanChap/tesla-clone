ad41739dea3e37649e4a49e7234c8664387923bd
Dean Chap <deanchaps@gmail.com>
2023-12-19 21:39:10 -0700

    Images commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,51 @@
 import React from 'react'
 import Model from './Model'
+import CyberTruckImg from '../assets/Cybertruck_74.jpg'
+import ModelYImg from '../assets/ModelY_88.jpg'
+import ModelXImg from '../assets/ModelX_73.jpg'
+import Model3Img from '../assets/Model3_82.jpg'
+import ModelSImg from '../assets/ModelS_74.jpg'
+import SolarImg from '../assets/Solar.jpg'
 
 const Home = () => {
   return (
     <div className='h-screen w-screen snap-y snap-mandatory overflow-y-scroll'>
       <Model 
-        title="Model 3"
+        title="Model S"
         leftBtn="Explore Inventory"
         rightBtn="Demo Drive"
-        />
+        bgImage={ModelSImg}
+      />
       <Model 
         title="Model X"
         leftBtn="Explore Inventory"
         rightBtn="Demo Drive"
-        />
+        bgImage={ModelXImg}
+      />
+      <Model 
+        title="Model 3"
+        leftBtn="Explore Inventory"
+        rightBtn="Demo Drive"
+        bgImage={Model3Img}
+      />
       <Model 
         title="Model Y"
         leftBtn="Explore Inventory"
         rightBtn="Demo Drive"
-        />
+        bgImage={ModelYImg}
+      />
       <Model 
-        title="Model S"
+        title="CyberTruck"
         leftBtn="Explore Inventory"
         rightBtn="Demo Drive"
-        />
+        bgImage={CyberTruckImg}
+      />
       <Model 
-        title="Experience Tesla"
-        description="Schedule a Demo drive today"
-        rightBtn="Demo Drive"
-        />
-      <Model 
-        title="Powerwall"
+        title="Solar and Powerwall"
         description="Power Everything"
         rightBtn="Learn more"
-        />
+        bgImage={SolarImg}
+      />
         
     </div>
   )
--- src/components/Model.js
+++ src/components/Model.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FiChevronDown } from "react-icons/fi";
 
-const Model = ({ title, description, leftBtn, rightBtn }) => {
+const Model = ({ title, description, leftBtn, rightBtn, bgImage }) => {
+  const [snapped, setSnapped] = useState(false);
+  const headerRef = useRef(null);
+  const titleRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const snapThreshold = 100;
+
+      if (!snapped && scrollPosition >= snapThreshold) {
+        setSnapped(true);
+        // Update the background image URL dynamically
+        // setBgImage("new-image-url-after-scroll.jpg");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [snapped]);
+
   return (
-    <div 
-      className="relative flex flex-col justify-between items-center h-screen bg-cover bg-center snap-start snap-always shrink-0
-      bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Desktop-NA.jpg')] 
-      lg:bg-[url('https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-3-Desktop-NA.jpg')]" 
+    <div
+      ref={headerRef}
+      className={`relative flex flex-col justify-between items-center h-screen bg-cover bg-center snap-start snap-always shrink-0`}
+      style={{
+        backgroundImage: bgImage ? `url('${bgImage}')` : null,
+        transition: "background-image 0.5s ease-in-out",
+      }}
     >
-      <div className="absolute inset-x-0 top-[15%] text-center text-white">
+      <div className="absolute inset-x-0 top-[15%] text-center" ref={titleRef}>
         <h1 className="text-4xl font-bold">{title}</h1>
         <p className="p-2 text-sm">{description}</p>
       </div>
 
-      <div className="flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]">
+      <div className="flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]" ref={buttonRef}>
         {leftBtn && (
           <a href="https://www.tesla.com/en_CA/inventory/new/my">
             <button className="bg-[#f4f4f4] rounded-md w-96 lg:w-64 lg:mx-4 h-10 mt-2">
@@ -23,7 +49,7 @@ const Model = ({ title, description, leftBtn, rightBtn }) => {
         )}
         {rightBtn && (
           <a href="https://www.tesla.com/en_ca/drive">
-            <button className="bg-[#393c41] rounded-md w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white">
+            <button className="bg-[#171a20]/90 rounded-md w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white">
               {rightBtn}
             </button>
           </a>
@@ -31,7 +57,7 @@ const Model = ({ title, description, leftBtn, rightBtn }) => {
       </div>
 
       <div className="flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]">
-        <FiChevronDown className="text-white" />
+        <FiChevronDown />
       </div>
     </div>
   );
